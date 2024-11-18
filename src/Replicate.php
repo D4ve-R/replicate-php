7dacb4d701e6cc12e10f1a8cf179c012d348b78b
Dave <69651599+D4ve-R@users.noreply.github.com>
2024-11-18 23:04:24 +0100

    add baseUrl to constructor

--- src/Replicate.php
+++ src/Replicate.php
@@ -13,13 +13,14 @@ final class Replicate extends Connector
 {
     public function __construct(
         public string $apiToken,
+        public string $baseUrl = 'https://api.replicate.com/v1'
     ) {
         $this->withTokenAuth($this->apiToken, 'Token');
     }
 
     public function resolveBaseUrl(): string
     {
-        return 'https://api.replicate.com/v1';
+        return $this->baseUrl;
     }
 
     protected function defaultHeaders(): array
