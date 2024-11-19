fe477736863ce4d436de82ff1b3aeb6b09549502
Dave <69651599+D4ve-R@users.noreply.github.com>
2024-11-19 21:18:36 +0100

    add HasPredictionData trait

---
+++ src/Traits/HasPredictionData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BenBjurstrom\Replicate\Traits;
+
+use BenBjurstrom\Replicate\Data\PredictionData;
+use BenBjurstrom\Replicate\Exceptions\InvalidDataException;
+use Saloon\Http\Response;
+
+
+trait HasPredictionData
+{
+    public function createDtoFromResponse(Response $response): PredictionData
+    {
+        return $this::fromResponse($response);
+    }
+
+    protected static function fromResponse(Response $response): PredictionData
+    {
+        $data = $response->json();
+        if (!is_array($data)) {
+            throw new InvalidDataException('Invalid response');
+        }
+
+        return new PredictionData(
+            id: $data['id'],
+            version: $data['version'],
+            createdAt: $data['created_at'],
+            completedAt: $data['completed_at'] ?? null,
+            startedAt: $data['started_at'] ?? null,
+            status: $data['status'],
+            webhookCompleted: $data['webhook_completed'] ?? null,
+            input: $data['input'],
+            metrics: $data['metrics'] ?? null,
+            urls: $data['urls'],
+            error: $data['error'] ?? null,
+            output: $data['output'] ?? null,
+        );
+    }
+}
